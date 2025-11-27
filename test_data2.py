6c2d9effb127528fe17df9f31efaa0ad51803c97
Xizun WANG <1120311927@qq.com>
2025-11-27 21:16:29 +0800

    "Clean"

--- test_data2.py
+++
@@ -1,8 +0,0 @@
-from src.data.data_fetcher import StockDataFetcher
-
-fetcher = StockDataFetcher()
-# 测试获取一只股票的数据
-stock_data = fetcher.get_stock_realtime_data('600000')
-print('测试股票 600000 数据:')
-for key, value in stock_data.items():
-    print(f'{key}: {value}')
